605193e36138cb2b05b1c151ecd9793aeba7dcbe
frank.yao <frank.yao@vipshop.com>
2014-02-21 12:07:14 +0800

    adjust tabsize from 4 to 2 and fix copyrights

--- src/main/java/org/apache/flume/sink/kafka/KafkaSink.java
+++ src/main/java/org/apache/flume/sink/kafka/KafkaSink.java
@@ -1,18 +1,21 @@
 /*******************************************************************************
- * Copyright 2013 Renjie Yao
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *  
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
 package org.apache.flume.sink.kafka;
 
 import kafka.javaapi.producer.Producer;
@@ -29,37 +32,42 @@ import org.apache.flume.sink.AbstractSink;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
- * A Sink of Kafka which get events from channels and publish to Kafka. I use this in our 
- * company production environment which can hit 100k messages per second.
- * <tt>zk.connect: </tt> the zookeeper ip kafka use.<p>
- * <tt>topic: </tt> the topic to read from kafka.<p>
- * <tt>batchSize: </tt> send serveral messages in one request to kafka. <p>
- * <tt>producer.type: </tt> type of producer of kafka, async or sync is available.<o>
- * <tt>serializer.class: </tt>{@kafka.serializer.StringEncoder}
+ * A Sink of Kafka which get events from channels and publish to Kafka. I use
+ * this in our company production environment which can hit 100k messages per
+ * second. <tt>zk.connect: </tt> the zookeeper ip kafka use.
+ * <p>
+ * <tt>topic: </tt> the topic to read from kafka.
+ * <p>
+ * <tt>batchSize: </tt> send serveral messages in one request to kafka.
+ * <p>
+ * <tt>producer.type: </tt> type of producer of kafka, async or sync is
+ * available.<o> <tt>serializer.class: </tt>{@kafka.serializer.StringEncoder
+ * 
+ * }
  */
-public class KafkaSink extends AbstractSink implements Configurable{
+public class KafkaSink extends AbstractSink implements Configurable {
 	private static final Logger log = LoggerFactory.getLogger(KafkaSink.class);
 	private String topic;
 	private Producer<String, String> producer;
-	
+
 	public Status process() throws EventDeliveryException {
 		Channel channel = getChannel();
 		Transaction tx = channel.getTransaction();
 		try {
 			tx.begin();
 			Event e = channel.take();
-			if(e == null) {
+			if (e == null) {
 				tx.rollback();
 				return Status.BACKOFF;
 			}
-			producer.send(new ProducerData<String, String>(topic, new String(e.getBody())));
+			producer.send(new ProducerData<String, String>(topic, new String(e
+					.getBody())));
 			log.trace("Message: {}", e.getBody());
 			tx.commit();
 			return Status.READY;
-		} catch(Exception e) {
-			log.error("KafkaSink Exception:{}",e);
+		} catch (Exception e) {
+			log.error("KafkaSink Exception:{}", e);
 			tx.rollback();
 			return Status.BACKOFF;
 		} finally {
@@ -69,7 +77,7 @@ public class KafkaSink extends AbstractSink implements Configurable{
 
 	public void configure(Context context) {
 		topic = context.getString("topic");
-		if(topic == null) {
+		if (topic == null) {
 			throw new ConfigurationException("Kafka topic must be specified.");
 		}
 		producer = KafkaSinkUtil.getProducer(context);
