ae0158891abbf4c459ae081dbd7dba68b4e1852c
Alan Ma <alan.ma@disney.com>
2014-04-20 11:16:03 -0700

    refactored kafkasink class

--- src/main/java/org/apache/flume/sink/kafka/KafkaSink.java
+++ src/main/java/org/apache/flume/sink/kafka/KafkaSink.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *  
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -18,7 +18,6 @@
  *******************************************************************************/
 package org.apache.flume.sink.kafka;
 
-import com.google.common.base.Preconditions;
 import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
 import org.apache.flume.*;
@@ -28,100 +27,90 @@ import org.apache.flume.sink.AbstractSink;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Properties;
-
 /**
  * A Sink of Kafka which get events from channels and publish to Kafka. I use
  * this in our company production environment which can hit 100k messages per
  * second. <tt>zk.connect: </tt> the zookeeper ip kafka use.
- * <p>
+ * <p/>
  * <tt>topic: </tt> the topic to read from kafka.
- * <p>
+ * <p/>
  * <tt>batchSize: </tt> send serveral messages in one request to kafka.
- * <p>
+ * <p/>
  * <tt>producer.type: </tt> type of producer of kafka, async or sync is
  * available.<o> <tt>serializer.class: </tt>{@kafka.serializer.StringEncoder
- * 
- * 
+ * <p/>
+ * <p/>
  * }
  */
 public class KafkaSink extends AbstractSink implements Configurable {
-	private static final Logger log = LoggerFactory.getLogger(KafkaSink.class);
-    private Properties parameters;
+    private static final Logger log = LoggerFactory.getLogger(KafkaSink.class);
     private String topic;
-	private Producer<String, String> producer;
+    //partition.key
+    private String partition;
+    private Producer<String, String> producer;
 
-    private static final String PARTITION_KEY_NAME = "custom.partition.key";
-    private static final String CUSTOME_TOPIC_KEY_NAME = "custom.topic.name";
     private static final String DEFAULT_ENCODING = "UTF-8";
 
-	public Status process() throws EventDeliveryException {
+
+    public Status process() throws EventDeliveryException {
         Status status = null;
         Channel channel = getChannel();
-		Transaction tx = channel.getTransaction();
-		try {
+        Transaction tx = channel.getTransaction();
+        try {
             tx.begin();
             Event event = channel.take();
-
-            if (event != null) {
-                String partitionKey = (String) parameters.get(PARTITION_KEY_NAME);
-                String topic = Preconditions.checkNotNull((String) this.parameters.get(CUSTOME_TOPIC_KEY_NAME),
-                        "topic name is required");
-                String eventData = new String(event.getBody(), DEFAULT_ENCODING);
-                KeyedMessage<String, String> data = (partitionKey.isEmpty()) ? new KeyedMessage<String, String>(topic,
-                        eventData) : new KeyedMessage<String, String>(topic, partitionKey, eventData);
-                log.info("Sending Message to Kafka : [" + topic + ":" + eventData + "]");
-                producer.send(data);
+            if (event == null) {
                 tx.commit();
-                log.info("Send message success");
-                status = Status.READY;
-            } else {
-                tx.rollback();
-                status = Status.BACKOFF;
-            }
+                status =  Status.BACKOFF;
 
-//			if (event == null) {
-//				tx.commit();
-//				return Status.READY;
-//
-//			}
-//			producer.send(new ProducerData<String, String>(topic, new String(event
-//					.getBody())));
-//			log.trace("Message: {}", event.getBody());
-//			tx.commit();
-//			return Status.READY;
-		} catch (Exception e) {
-			try {
-				tx.rollback();
-				status = Status.BACKOFF;
-			} catch (Exception e2) {
-				log.error("Rollback Exception:{}", e2);
-			}		
-			log.error("KafkaSink Exception:{}", e);
-            status =  Status.BACKOFF;
-		} finally {
-			tx.close();
-		}
+            } else {
+
+                String eventData = new String(event.getBody(), DEFAULT_ENCODING);
+                KeyedMessage<String, String> data = (partition.isEmpty()) ? new KeyedMessage<String, String>(topic,
+                        eventData) : new KeyedMessage<String, String>(topic, partition, eventData);
+
+                producer.send(data);
+                log.info("Sending Message to Kafka : [" + topic + ":" + eventData + "]");
+
+                tx.commit();
+                status = Status.READY;
+                log.info("Send message success");
+            }
+        } catch (Exception e) {
+            try {
+                tx.rollback();
+                return Status.BACKOFF;
+            } catch (Exception e2) {
+                log.error("Rollback Exception:{}", e2);
+            }
+            log.error("KafkaSink Exception:{}", e);
+            return Status.BACKOFF;
+        } finally {
+            tx.close();
+        }
 
         return status;
-	}
+    }
 
-	public void configure(Context context) {
-		topic = context.getString("topic");
-		if (topic == null) {
-			throw new ConfigurationException("Kafka topic must be specified.");
-		}
-		producer = KafkaSinkUtil.getProducer(context);
-	}
+    public void configure(Context context) {
+        topic = context.getString("topic");
+        if (topic == null) {
+            throw new ConfigurationException("Kafka topic must be specified.");
+        }
 
-	@Override
-	public synchronized void start() {
-		super.start();
-	}
+        partition = context.getString("partition");
 
-	@Override
-	public synchronized void stop() {
-		producer.close();
-		super.stop();
-	}
+        producer = KafkaSinkUtil.getProducer(context);
+    }
+
+    @Override
+    public synchronized void start() {
+        super.start();
+    }
+
+    @Override
+    public synchronized void stop() {
+        producer.close();
+        super.stop();
+    }
 }
